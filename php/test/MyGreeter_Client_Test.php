d189fdc96a1ea954f391fcba01e5aa8a3a3f1328
yowcowvg <yoko_ohyama@voyagegroup.com>
2015-07-28 18:32:31 +0900

    add Makefile and test

---
+++ php/test/MyGreeter_Client_Test.php
@@ -0,0 +1,25 @@
+<?php
+
+class MyGreeter_Client_Test extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->greeter = new \MyGreeter\Client();
+    }
+
+    public function test_Instance()
+    {
+        $this->assertEquals(
+            get_class($this->greeter),
+            'MyGreeter\Client'
+        );
+    }
+
+    public function test_getGreeting()
+    {
+        $this->assertEquals(
+            $this->greeter->getGreeting(),
+            'Good morning'
+        );
+    }
+}
