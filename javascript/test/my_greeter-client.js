641d5da58948a3797ef37fbb30dc5c595b627c6c
t-sekiguchi <takafumi.sekiguchi@mkt-apps.com>
2015-07-28 19:56:28 +0900

    js-beautify

--- javascript/test/my_greeter-client.js
+++ javascript/test/my_greeter-client.js
@@ -4,11 +4,11 @@ var assert = require('assert');
 var MyGreeter = require('../src/MyGreeter.js');
 
 it('instance', function() {
-  var greeter = new MyGreeter.Client();
-  assert.ok(greeter instanceof MyGreeter.Client);
+    var greeter = new MyGreeter.Client();
+    assert.ok(greeter instanceof MyGreeter.Client);
 });
 
 it('getGreeting', function() {
-  var greeter = new MyGreeter.Client();
-  assert.strictEqual(greeter.getGreeting(), 'Good morning');
+    var greeter = new MyGreeter.Client();
+    assert.strictEqual(greeter.getGreeting(), 'Good morning');
 });
