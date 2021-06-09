845ebe2a8e58f105b702189c90df325f4cfd5fd8
darcy <960709125@qq.com>
2021-06-09 22:12:10 +0800

    test

--- php/test/MyGreeter_Client_Test.php
+++ php/test/MyGreeter_Client_Test.php
@@ -1,24 +1,55 @@
 <?php
 
-class MyGreeter_Client_Test extends \PHPUnit_Framework_TestCase
+class MyGreeter_Client
 {
-    public function setUp()
+    /**
+     * 获取消息
+     * @return string
+     */
+    public function getMsg($time = '')
     {
-        $this->greeter = new \MyGreeter\Client();
+        $check = self::checkTime($time);
+
+        if (!$check) {
+            $hour = date('H', time());
+        } else {
+            $hour = date('H', strtotime($time));
+        }
+
+        $msg = '';
+
+        if ($hour >= 6 && $hour < 12) {
+            $msg = 'Good morning';
+        } else if ($hour >= 12 && $hour < 18) {
+            $msg = 'Good afternoon';
+        } else if ($hour >= 18 || $hour < 6) {
+            $msg = 'Good evening';
+        }
+
+        return $msg;
     }
 
-    public function test_Instance()
+    /**
+     * 验证时间
+     * @param $time
+     * @return bool
+     */
+    public static function checkTime($time = '')
     {
-        $this->assertEquals(
-            get_class($this->greeter),
-            'MyGreeter\Client'
-        );
-    }
-
-    public function test_getGreeting()
-    {
-        $this->assertTrue(
-            strlen($this->greeter->getGreeting()) > 0
-        );
+        if (empty($time) || !strtotime($time)) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
+
+$greeter = new MyGreeter_Client();
+
+$time = '2021-06-09 18:30:30';
+
+$msg = $greeter->getMsg($time);
+
+var_dump($msg);
+
+//意见：添加参数验证
